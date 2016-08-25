4a05488bad7cb61d9f37f152fe154d4d20cfc1b3
钱龙 <QianL@QianLong.local>
2016-08-25 11:02:27 +0800

    代码优化

--- src/main/java/com/yiji/falcon/agent/plugins/plugin/tcp/TcpPlugin.java
+++ src/main/java/com/yiji/falcon/agent/plugins/plugin/tcp/TcpPlugin.java
@@ -19,7 +19,6 @@ import java.io.IOException;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.Socket;
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Map;
