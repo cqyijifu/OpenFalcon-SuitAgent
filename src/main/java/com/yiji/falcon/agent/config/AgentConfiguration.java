8095ba4c37fbf4b9e3dfda73cb90b240cc0161e8
QianLong <642321251@qq.com>
2016-10-19 14:13:44 +0800

    修复SNMP采集时，当SNMP设备不可用时的采集异常问题

--- src/main/java/com/yiji/falcon/agent/config/AgentConfiguration.java
+++ src/main/java/com/yiji/falcon/agent/config/AgentConfiguration.java
@@ -27,7 +27,7 @@ public enum  AgentConfiguration {
 
     private final Logger log = LoggerFactory.getLogger(this.getClass());
 
-    public static final float VERSION = (float) 4.0;
+    public static final float VERSION = (float) 4.1;
 
     /**
      * quartz配置文件路径
