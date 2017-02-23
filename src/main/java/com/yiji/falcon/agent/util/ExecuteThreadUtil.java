0f9596b2ebbba21de252cb010b053703c0cb40e9
QianLong <642321251@qq.com>
2017-02-23 18:31:21 +0800

    改变线程池满后的拒绝策略
    Signed-off-by: QianLong <642321251@qq.com>

--- src/main/java/com/yiji/falcon/agent/util/ExecuteThreadUtil.java
+++ src/main/java/com/yiji/falcon/agent/util/ExecuteThreadUtil.java
@@ -17,41 +17,44 @@ import java.util.concurrent.*;
  */
 public class ExecuteThreadUtil {
     private static ExecutorService executorService;
+
     static {
         final int maxPoolSize = AgentConfiguration.INSTANCE.getAgentMaxThreadCount();
         //定义并发执行服务
-        executorService = new ThreadPoolExecutor(5,maxPoolSize,0L,TimeUnit.MILLISECONDS,
+        executorService = new ThreadPoolExecutor(5, maxPoolSize, 0L, TimeUnit.MILLISECONDS,
                 new SynchronousQueue<>(),
                 r -> {
-                    Thread t=new Thread(r);
+                    Thread t = new Thread(r);
                     t.setName("agentThreadPool");
                     return t;
-                }
+                },new ThreadPoolExecutor.DiscardOldestPolicy()
         );
     }
 
     /**
      * 执行线程任务
+     *
      * @param task
      */
-    public static void execute(Runnable task){
+    public static void execute(Runnable task) {
         executorService.submit(task);
     }
 
     /**
      * 执行线程任务
+     *
      * @param task
      * @param <T>
      * @return
      */
-    public static <T> Future<T> execute(Callable<T> task){
+    public static <T> Future<T> execute(Callable<T> task) {
         return executorService.submit(task);
     }
 
     /**
      * 关闭线程池
      */
-    public static void shutdown(){
+    public static void shutdown() {
         executorService.shutdown();
     }
 }
